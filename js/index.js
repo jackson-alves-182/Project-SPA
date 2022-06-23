85ff14a43ba82e605ef2d003df186acb47f1d7bf
Jackson Alves <jack182@outlook.com.br>
2022-06-23 19:13:26 -0300

    finished some details in the header, and started with routes in JS

---
+++ js/index.js
@@ -0,0 +1,9 @@
+import { Router } from './router.js'
+
+const router = new Router();
+
+router.add('/',  "./pages/universe.html");
+router.add("/universe", "./pages/universe.html");
+router.add("/exploration", "./pages/exploration.html");
+
+
